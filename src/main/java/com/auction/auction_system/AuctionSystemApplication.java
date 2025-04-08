9274a6733e5c5f9404f586882c20201e8617712a
Vanshaj Saxena <vs110405@outlook.com>
2025-04-08 21:17:06 +0530

    format: all

--- src/main/java/com/auction/auction_system/AuctionSystemApplication.java
+++ src/main/java/com/auction/auction_system/AuctionSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuctionSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuctionSystemApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(AuctionSystemApplication.class, args);
+  }
 
 }
