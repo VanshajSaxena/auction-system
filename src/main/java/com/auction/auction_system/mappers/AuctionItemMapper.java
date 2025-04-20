0884d0df42f7784b3639309daab1afedb538284c
Vanshaj Saxena <vs110405@outlook.com>
2025-04-20 05:57:29 +0530

    setup mappings

--- src/main/java/com/auction/auction_system/mappers/AuctionItemMapper.java
+++ src/main/java/com/auction/auction_system/mappers/AuctionItemMapper.java
@@ -1,6 +1,7 @@
 package com.auction.auction_system.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import com.auction.auction_system.entities.AuctionItemEntity;
@@ -9,6 +10,7 @@ import com.auction.auction_system.generated.models.AuctionItemDto;
 @Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.WARN)
 public interface AuctionItemMapper {
 
+  @Mapping(target = "ownerId", source = "owner.id")
   AuctionItemDto toDto(AuctionItemEntity auctionItemEntity);
 
 }
--- src/main/java/com/auction/auction_system/mappers/AuctionListingMapper.java
+++ src/main/java/com/auction/auction_system/mappers/AuctionListingMapper.java
@@ -1,6 +1,9 @@
 package com.auction.auction_system.mappers;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import com.auction.auction_system.entities.AuctionListingEntity;
@@ -9,6 +12,11 @@ import com.auction.auction_system.generated.models.AuctionListingDto;
 @Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.WARN)
 public interface AuctionListingMapper {
 
+  @Mapping(target = "creatorId", source = "creator.id")
   AuctionListingDto toDto(AuctionListingEntity auctionListingEntity);
+
+  List<AuctionListingDto> toDtoList(List<AuctionListingEntity> auctionListingEntityList);
+
+  AuctionListingEntity toEntity(AuctionListingDto auctionListingDto);
 
 }
