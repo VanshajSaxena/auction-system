5a8621421c65fbe1d5f592bfc7adc411c368d32c
Vanshaj Saxena <vs110405@outlook.com>
2025-04-22 16:57:36 +0530

    feat(repositories): create entity repositories for remaining entities

---
+++ src/main/java/com/auction/auction_system/repositories/BidRepository.java
@@ -0,0 +1,11 @@
+package com.auction.auction_system.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.auction.auction_system.entities.BidEntity;
+
+@Repository
+public interface BidRepository extends JpaRepository<BidEntity, Long> {
+
+}
