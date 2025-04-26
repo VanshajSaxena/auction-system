884c420578d87bdfd9bc482e61af692518a281d8
Vanshaj Saxena <vs110405@outlook.com>
2025-04-26 15:01:41 +0530

    feat(repositories): method to query user repository by username

--- src/main/java/com/auction/auction_system/repositories/UserRepository.java
+++ src/main/java/com/auction/auction_system/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.auction.auction_system.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.auction.auction_system.entities.UserEntity;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
+  Optional<UserEntity> findByUsername(String username);
 }
