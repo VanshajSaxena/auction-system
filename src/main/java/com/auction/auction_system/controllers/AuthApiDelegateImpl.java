0ebdebef8e87b7c3e953bea50f03d32fbadb2cea
Vanshaj Saxena <vs110405@outlook.com>
2025-04-29 01:10:22 +0530

    fix(controllers): update delegate implementation for auth controller

--- src/main/java/com/auction/auction_system/controllers/AuthApiDelegateImpl.java
+++ src/main/java/com/auction/auction_system/controllers/AuthApiDelegateImpl.java
@@ -1,22 +1,40 @@
 package com.auction.auction_system.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.auction.auction_system.generated.controllers.AuthApiDelegate;
+import com.auction.auction_system.generated.models.TokensDto;
 import com.auction.auction_system.generated.models.UserLoginRequestDto;
-import com.auction.auction_system.generated.models.UserLoginResponseDto;
 import com.auction.auction_system.generated.models.UserRegistrationRequestDto;
 import com.auction.auction_system.generated.models.UserRegistrationResponseDto;
+import com.auction.auction_system.services.AuthenticationService;
 
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class AuthApiDelegateImpl implements AuthApiDelegate {
 
+  private final AuthenticationService authenticationService;
+
   @Override
-  public ResponseEntity<UserLoginResponseDto> login(UserLoginRequestDto userLoginRequestDto) {
-    // TODO: Implement after security configuration.
-    return AuthApiDelegate.super.login(userLoginRequestDto);
+  @PostMapping("/login")
+  public ResponseEntity<TokensDto> login(UserLoginRequestDto userLoginRequestDto) {
+    UserDetails userDetails = authenticationService.authenticate(userLoginRequestDto.getUsername(),
+        userLoginRequestDto.getEmail(), userLoginRequestDto.getPassword());
+
+    String accessToken = authenticationService.generateToken(userDetails);
+
+    return ResponseEntity.ok(TokensDto.builder().accessToken(accessToken).build());
   }
 
   @Override
+  @PostMapping("/register")
   public ResponseEntity<UserRegistrationResponseDto> register(UserRegistrationRequestDto userRegistrationRequestDto) {
     // TODO: Implement after security configuration.
     return AuthApiDelegate.super.register(userRegistrationRequestDto);
