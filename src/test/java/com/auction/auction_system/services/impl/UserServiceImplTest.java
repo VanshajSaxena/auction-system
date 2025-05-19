4797e7d9ca9a95960167212857047a7fe8635216
Vanshaj Saxena <vs110405@outlook.com>
2025-05-19 23:00:17 +0530

    feat(tests): test `UserServiceImpl` registerUser method

--- src/test/java/com/auction/auction_system/services/impl/UserServiceImplTest.java
+++ src/test/java/com/auction/auction_system/services/impl/UserServiceImplTest.java
@@ -1,19 +1,27 @@
 package com.auction.auction_system.services.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.auction.auction_system.entities.UserEntity;
+import com.auction.auction_system.exception.UsernameAlreadyExistsException;
 import com.auction.auction_system.generated.models.UserDto;
+import com.auction.auction_system.generated.models.UserRegistrationRequestDto;
+import com.auction.auction_system.generated.models.UserRegistrationResponseDto;
 import com.auction.auction_system.mappers.UserMapper;
 import com.auction.auction_system.repositories.UserRepository;
 import com.auction.auction_system.testutils.UserServiceTestDataFactory;
@@ -26,6 +34,9 @@ public class UserServiceImplTest {
 
   @Mock
   private UserMapper userMapper;
+
+  @Mock
+  private PasswordEncoder passwordEncoder;
 
   @InjectMocks
   UserServiceImpl underTest;
@@ -44,5 +55,52 @@ public class UserServiceImplTest {
     assertEquals(mockDtoList, result);
     verify(userRepository).findAll();
     verify(userMapper).toDtoList(mockEntityList);
+  }
+
+  @Test
+  public void givenValidRegistrationDto_whenRegisterUser_returnSaveUserAndReturnResponse() {
+    // Arrange
+    UserRegistrationRequestDto requestDto = UserServiceTestDataFactory.createUserRegistrationRequestDto();
+    UserEntity userEntity = UserServiceTestDataFactory.createUserEntityFromUserRegistrationRequestDto(requestDto);
+    UserEntity savedUserEntity = UserServiceTestDataFactory.createSavedUserEntityWithId(userEntity);
+
+    when(userRepository.findByUsername(requestDto.getUsername())).thenReturn(Optional.empty());
+    when(userRepository.findByEmail(requestDto.getEmail())).thenReturn(Optional.empty());
+    when(passwordEncoder.encode(requestDto.getPassword())).thenReturn("encodedPassword");
+    when(userRepository.save(any(UserEntity.class))).thenReturn(savedUserEntity);
+
+    // Act
+    UserRegistrationResponseDto responseDto = underTest.registerUser(requestDto);
+
+    // Assert
+    assertNotNull(responseDto.getUserId());
+    assertEquals(savedUserEntity.getId(), responseDto.getUserId());
+    verify(userRepository).findByUsername(requestDto.getUsername());
+    verify(userRepository).findByEmail(requestDto.getEmail());
+    verify(passwordEncoder).encode(requestDto.getPassword());
+    verify(userRepository).save(any(UserEntity.class));
+  }
+
+  @Test
+  public void givenExistingUsername_whenRegisterUser_thenThrowUsernameAlreadyExistsException() {
+    // Arrange
+    UserRegistrationRequestDto requestDto = UserServiceTestDataFactory.createUserRegistrationRequestDto();
+    UserEntity existingUserEntity = UserServiceTestDataFactory
+        .createUserEntityFromUserRegistrationRequestDto(requestDto);
+
+    when(userRepository.findByUsername(requestDto.getUsername())).thenReturn(Optional.of(existingUserEntity));
+
+    // Act & Assert
+    UsernameAlreadyExistsException exception = assertThrows(UsernameAlreadyExistsException.class, () -> {
+      underTest.registerUser(requestDto);
+    });
+
+    assertEquals("The username '%s' already exists.".formatted(requestDto.getUsername()), exception.getMessage());
+    verify(userRepository).findByUsername(requestDto.getUsername());
+  }
+
+  @Test
+  public void givenExistingEmail_whenRegisterUser_thenThrowEmailAlreadyExistsException() {
+
   }
 }
