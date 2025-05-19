1b0805acb71d9380e347a68912d8a000efabf9fa
Vanshaj Saxena <vs110405@outlook.com>
2025-05-19 23:01:05 +0530

    fix(exception): call the super class constructor to inheret the behaviour properly

--- src/main/java/com/auction/auction_system/exception/AuctionApplicationException.java
+++ src/main/java/com/auction/auction_system/exception/AuctionApplicationException.java
@@ -1,12 +1,20 @@
 package com.auction.auction_system.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
 public abstract class AuctionApplicationException extends RuntimeException {
 
-  private String message;
+  public AuctionApplicationException() {
+    super();
+  }
 
+  public AuctionApplicationException(String message) {
+    super(message);
+  }
+
+  public AuctionApplicationException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public AuctionApplicationException(Throwable cause) {
+    super(cause);
+  }
 }
