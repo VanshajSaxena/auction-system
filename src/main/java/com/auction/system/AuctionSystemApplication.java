010d75a1cc1413052bb33fd1450aff5dc698e734
Vanshaj Saxena <vs110405@outlook.com>
2025-05-29 18:18:24 +0530

    feat: add a print statement to see application args

--- src/main/java/com/auction/system/AuctionSystemApplication.java
+++ src/main/java/com/auction/system/AuctionSystemApplication.java
@@ -1,5 +1,7 @@
 package com.auction.system;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuctionSystemApplication {
 
   public static void main(String[] args) {
+    System.out.println("Application args: " + Arrays.toString(args));
     SpringApplication.run(AuctionSystemApplication.class, args);
   }
 
