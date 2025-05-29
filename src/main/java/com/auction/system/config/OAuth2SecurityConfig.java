e9afd246e43db70886dda31918d019bed88b5026
Vanshaj Saxena <vs110405@outlook.com>
2025-05-29 21:52:09 +0530

    style(config): inline getDecoder method

--- src/main/java/com/auction/system/config/OAuth2SecurityConfig.java
+++ src/main/java/com/auction/system/config/OAuth2SecurityConfig.java
@@ -3,12 +3,13 @@ package com.auction.system.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class OAuth2SecurityConfig {
 
   @Bean
@@ -16,14 +17,11 @@ public class OAuth2SecurityConfig {
       throws Exception {
     return http
         .securityMatcher("/api/v1/auth/google/callback", "/api/v1/auth/apple/callback")
-
         .csrf(csrf -> csrf.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .oauth2ResourceServer((oauth2) -> oauth2.jwt((jwt) -> jwt.decoder(getDecoder())))
+        .oauth2ResourceServer((oauth2) -> oauth2
+            .jwt((jwt) -> jwt
+                .decoder(JwtDecoders.fromIssuerLocation("https://accounts.google.com"))))
         .build();
-  }
-
-  private JwtDecoder getDecoder() {
-    return JwtDecoders.fromIssuerLocation("https://accounts.google.com");
   }
 }
